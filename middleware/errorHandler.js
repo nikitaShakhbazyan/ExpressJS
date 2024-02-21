d4709bc1f3aa9a6fe7b0f5f5163a3177530fcbbf
nickshakh <shakhbazyann@gmail.com>
2024-02-21 14:31:53 +0400

    Modified switch case statement and created Constants

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,19 +1,41 @@
+const {constants} = require('../constants')
+
 const errorHandler = (err,req,res,next) => {
     const status = res.status ? res.status : 500;
 
     switch (status) {
-        case 400:
+        case constants.VALIDATION_ERROR:
             res.json({title : "Validation Failed",
             message : err.message,
             stackTrace : err.stack
         })
             break;
-        case 404 :
+        case constants.NOT_FOUND :
             res.json({title : "Not Found",
             message : err.message,
             stackTrace : err.stack
         })
+            break
+        case constants.FORBIDDEN :
+            res.json({title : "Forbidden",
+            message : err.message,
+            stackTrace : err.stack
+        })
+            break
+        case constants.UNAUTHORIZED :
+            req.json({title:"Unauthorized",
+            message: err.message,
+            stackTrace:err.stack
+        })
+            break 
+        case constants.SERVER_ERROR :
+            req.json({title:"Server Error",
+            message:err.message,
+            stackTrace:err.stack
+        })
+            break
         default:
+            console.log("There is no errors!!")
             break;
     }
 
