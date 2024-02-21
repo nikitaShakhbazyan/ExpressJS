a1844b8f99d29a0bd28e12ba608376c010fff772
nickshakh <shakhbazyann@gmail.com>
2024-02-22 02:19:08 +0400

    added JWT access

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const User = require("../models/userModel")
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 //@public route,POST method
 const registerUser = asyncHandler( async (req,res) => {
@@ -37,7 +38,27 @@ const registerUser = asyncHandler( async (req,res) => {
 })
 
 //@public route,POST method
-const loginUser = asyncHandler((req,res) => {
+const loginUser = asyncHandler( async (req,res) => {
+    const {email,password} = req.body;
+    if(!email || !password) {
+        res.status(400);
+        throw new Error ("All fields are mandatory!)")
+    }
+    const user = await User.findOne({email})
+    // compare pass with hashepass
+    if(user && (await bcrypt.compare(password,user.password))) {
+        const accessToken = jwt.sign({
+            user : {
+                username: user.username,
+                email: user.email,
+                id : user.id
+            },
+        },process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1m'}   )
+        res.status(200).json({accessToken});
+    } else {
+        res.status(401)
+        throw new Error("Email or pass is not valid")
+    }
     res.json({message:"Login the user"})
 })
 
