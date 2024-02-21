28926d93be2c7bd5eac5d1e153d63cd2065f1133
nickshakh <shakhbazyann@gmail.com>
2024-02-21 16:10:28 +0400

    added schema

---
+++ models/contactModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const contactSchema = mongoose.Schema({
+    name : {
+        type: String,
+        required : [true,'Please add name']
+    },
+    email : {
+        type: String,
+        required : [true,'Please add your email']
+    },
+    phone : {
+        type : String,
+        required : [true,'Please add your phone']
+    }
+}, {
+    timestamps : true
+})
+
+module.exports = mongoose.model('Contact', contactSchema)
