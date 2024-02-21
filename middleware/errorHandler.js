a1f818a9aa09ad4f23878ac5b5d0db2d5053f0b6
nickshakh <shakhbazyann@gmail.com>
2024-02-21 14:20:56 +0400

    added switch case statement

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,23 @@
 const errorHandler = (err,req,res,next) => {
     const status = res.status ? res.status : 500;
-    res.json({message : err.message, stackTrace : err.stack})
+
+    switch (status) {
+        case 400:
+            res.json({title : "Validation Failed",
+            message : err.message,
+            stackTrace : err.stack
+        })
+            break;
+        case 404 :
+            res.json({title : "Not Found",
+            message : err.message,
+            stackTrace : err.stack
+        })
+        default:
+            break;
+    }
+
+
 }
 
 module.exports = errorHandler
