f32c38eae635a121b9dbadc29fe2c2be3c4760a9
nickshakh <shakhbazyann@gmail.com>
2024-02-21 13:13:14 +0400

    Added routes and methods

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const dotenv = require('dotenv').config()
+const routesFromContactRoutes =  require('./routes/contactRoutes')
 
 const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
+
+app.use('/api/contacts',routesFromContactRoutes)
 
 app.listen(port,() => {
     console.log(`Server is running on port ${port} `)
